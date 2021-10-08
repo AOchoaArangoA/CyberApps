11479bc62a1f0835927b3247f720a06dae96ab37
Edwin A. Bermudez Ardila <edwinbermudez117@gmail.com>
2021-10-07 23:18:40 -0500

    Se Crearon los Componentes Header - SideMenu

--- cyberapps/src/App.js
+++ cyberapps/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+
+
+/* Componentes */
+import Header from './components/Header';
+import SideMenu from './components/SideMenu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App sb-nav-fixed">
+      <Header />
+
+      <div id="layoutSidenav">
+      <SideMenu />
+      
+      </div>
     </div>
   );
 }
---
+++ cyberapps/src/components/SideMenu.js
@@ -0,0 +1,83 @@
+import React from 'react'
+
+const SideMenu = () => {
+    return (
+        <div id="layoutSidenav_nav">
+            <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div className="sb-sidenav-menu">
+                    <div className="nav">
+                        <div className="sb-sidenav-menu-heading">Inicio</div>
+                        <a className="nav-link" href="index.html">
+                            <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
+                            Tablero
+                        </a>
+                        <div className="sb-sidenav-menu-heading">Interfaces</div>
+                        <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseVentas" aria-expanded="false" aria-controls="collapseVentas">
+                            <div className="sb-nav-link-icon"><i className="fas fa-columns"></i></div>
+                            Ventas
+                            <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
+                        </a>
+                        <div className="collapse" id="collapseVentas" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav className="sb-sidenav-menu-nested nav">
+                                <a className="nav-link" href="crearventa.html">Ventas</a>
+                                <a className="nav-link" href="listaVentas.html">Informe</a>
+                            </nav>
+                        </div>
+                        <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseProductos" aria-expanded="false" aria-controls="collapseProductos">
+                            <div className="sb-nav-link-icon"><i className="fas fa-columns"></i></div>
+                            Productos
+                            <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
+                        </a>
+                        <div className="collapse" id="collapseProductos" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav className="sb-sidenav-menu-nested nav">
+                                <a className="nav-link" href="#">Productos</a>
+                                <a className="nav-link" href="listaProductos.html">Informe Productos</a>
+                            </nav>
+                        </div>
+                        <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseVendedores" aria-expanded="false" aria-controls="collapseVendedores">
+                            <div className="sb-nav-link-icon"><i className="fas fa-columns"></i></div>
+                            Vendedores
+                            <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
+                        </a>
+                        <div className="collapse" id="collapseVendedores" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav className="sb-sidenav-menu-nested nav">
+                                <a className="nav-link" href="#">Vendedores</a>
+                                <a className="nav-link" href="listaVendedores.html">Listar Vendedores</a>
+                            </nav>
+                        </div>
+
+                        <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseClientes" aria-expanded="false" aria-controls="collapseClientes">
+                            <div className="sb-nav-link-icon"><i className="fas fa-columns"></i></div>
+                            Clientes
+                            <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
+                        </a>
+                        <div className="collapse" id="collapseClientes" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav className="sb-sidenav-menu-nested nav">
+                                <a className="nav-link" href="crearCliente.html">Clientes</a>
+                                <a className="nav-link" href="listaClientes.html">Listar Clientes</a>
+                            </nav>
+                        </div>
+                        <div className="sb-sidenav-menu-heading">Complementos</div>
+                        <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUsuario" aria-expanded="false" aria-controls="collapseUsuario">
+                            <div className="sb-nav-link-icon"><i className="fas fa-columns"></i></div>
+                            Usuarios
+                            <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
+                        </a>
+                        <div className="collapse" id="collapseUsuario" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav className="sb-sidenav-menu-nested nav">
+                                <a className="nav-link" href="#">Usuarios</a>
+                                <a className="nav-link" href="listaUsuarios.html">Listar Usuarios</a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div className="sb-sidenav-footer">
+                    <div className="small">Usuario:</div>
+                    Start Bootstrap
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default SideMenu
